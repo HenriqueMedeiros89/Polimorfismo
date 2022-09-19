68854c6d4cb322bf06089524fefb677364e26d89
Henrique Medeiros <henrique.medeiros@alunos.sc.senac.br>
2022-09-19 20:29:03 -0300

    adicao dos outros metodos a main class

--- src/polimorfismo/Polimorfismo.java
+++ src/polimorfismo/Polimorfismo.java
@@ -5,7 +5,9 @@ public class Polimorfismo {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
         
-        System.out.println("Resultado: " + (calc.somar(2, 3, 6)));
+//        System.out.println("Resultado: " + (calc.somar(2, 3, 6)));
+        System.out.println("Resultado: " + (calc.somar(2, 5)));
+        System.out.println("Resultado: " + (calc.somar(9, 5)));
 
     }
 
