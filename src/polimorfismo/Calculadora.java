58745aea1bab19069cfe1bb9d095c6aa21a758df
Henrique Medeiros <henrique.medeiros@alunos.sc.senac.br>
2022-09-19 20:20:15 -0300

    Polimorfismo

---
+++ src/polimorfismo/Calculadora.java
@@ -0,0 +1,17 @@
+package polimorfismo;
+
+public class Calculadora {
+
+    public int somar(int v1, int v2) {
+
+        int soma = v1 + v2;
+        return soma;
+    }
+
+    public int somar(int v1, int v2, int v3) {
+
+        int soma = v1 + v2 + v3;
+        return soma;
+    }
+
+}
