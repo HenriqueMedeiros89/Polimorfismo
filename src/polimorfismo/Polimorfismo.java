49041306ebce81888d3c356640e40c3417332a89
Henrique Medeiros <henrique.medeiros@alunos.sc.senac.br>
2022-09-19 20:27:26 -0300

    adicao de metodo de impressao do resultado dos metodos de soma

--- src/polimorfismo/Polimorfismo.java
+++ src/polimorfismo/Polimorfismo.java
@@ -3,6 +3,9 @@ package polimorfismo;
 public class Polimorfismo {
 
     public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        
+        System.out.println("Resultado: " + (calc.somar(2, 3, 6)));
 
     }
 
